ec43a3129dfa7c7d1b789aa46ae67093cf8c9c13
Leihao Chen <47678767+leihchen@users.noreply.github.com>
2021-08-26 12:36:34 -0400

    Rewrite Matan's installation scripts

--- scripts/postgres_interface.py
+++ scripts/postgres_interface.py
@@ -49,8 +49,17 @@ def get_history_data():
 
         cursor.execute("select file_names from history")
         history = cursor.fetchone()
-        history=history[0]
-
+​
+        if not history is None:
+            history=history[0]
+        else:
+            cursor.execute("INSERT INTO HISTORY (file_names) VALUES ('test.tar.gz')")
+            conn.commit() 
+            cursor.execute("select file_names from history")
+            history = cursor.fetchone()
+            history=history[0]
+​
+​
         cursor.close()
         conn.close()
     except Exception as ex:
