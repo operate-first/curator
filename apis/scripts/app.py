e42560934ee01b521c20aac5ee5b59aeecf57e5d
Leihao Chen <47678767+leihchen@users.noreply.github.com>
2021-09-24 18:00:53 -0400

    add Report type server

--- apis/scripts/app.py
+++ apis/scripts/app.py
@@ -1,15 +1,24 @@
 from flask import Flask, request, jsonify
 from postgres_interface import postgres_execute
+from kubernetes import client, config
+from openshift.dynamic import DynamicClient
 
 app = Flask(__name__)
 
 
 @app.route('/report')
 def report():
-    sql = 'select * from reports where 1 = 1 '
-    for k, v in request.args.items():
-        sql += " AND {} = '{}' ".format(k, v)
-    print(sql)
+    config.load_incluster_config()
+    k8s_client = client.ApiClient()
+    dyn_client = DynamicClient(k8s_client)
+    report = dyn_client.resources.get(api_version='batch.curator.openshift.io/v1', kind='Report')
+    resp = report.get(name=request.args['reportName'], namespace=request.args['reportNamespace'])
+    print(resp)
+    sql = "select * from reports where namespace={} and frequency= '{}'".format(resp['spec']['namespace'],
+                                                                                resp['spec']['reportPeriod'])
+    # for k, v in request.args.items():
+    #     sql += " AND {} = '{}' ".format(k, v)
+    # print(sql)
     table = postgres_execute(sql, result=True)
     print(table)
     return jsonify(table)
