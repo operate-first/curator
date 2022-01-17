2b6532fc88326323e117310be36f19e641989c3e
Leihao Chen <47678767+leihchen@users.noreply.github.com>
2022-01-17 10:20:03 -0600

    Update sanity_check.py

--- testing/sanity_check.py
+++ testing/sanity_check.py
@@ -9,9 +9,12 @@ warnings.filterwarnings("ignore")
 database_name = ''
 database_user = ''
 database_password = ''
-database_host_name = ''
+database_host_name = 'localhost'
 port = ''
 bearer_token = "sha256~xx"
+target_prometheus = "http://prometheus-k8s-openshift-monitoring.apps.smaug.na.operate-first.cloud"
+
+REL_TOLERANCE = .05  # relative tolerance
 
 
 def postgres_execute(sql_query, data=None, result=False, header=False):
@@ -50,7 +53,8 @@ def postgres_execute(sql_query, data=None, result=False, header=False):
         conn.close()
         return result_list
 
-def isclose(a, b, rel_tol=.05):
+
+def isclose(a, b, rel_tol=REL_TOLERANCE):
     return abs(a-b) <= rel_tol * max(abs(a), abs(b))
 
 
@@ -60,45 +64,71 @@ s = offset.seconds
 hours, remainder = divmod(s, 3600)
 minutes, seconds = divmod(remainder, 60)
 offset_str = "{}h{}m{}s{}ms".format(hours, minutes, seconds, offset.microseconds // 1000)
-# $ oc project openshift-monitoring; oc get routes
-curator_url = "http://prometheus-k8s-openshift-monitoring.apps.curator.massopen.cloud"
-auth_header = {"Authorization": "Bearer " + bearer_token}
-prom = PrometheusConnect(url=curator_url, headers=auth_header, disable_ssl=True)
-dbresp = postgres_execute("SELECT * FROM reports_human WHERE interval_end = '{}' and frequency = 'day'"
-                          .format(start_of_today), result=True)
-print('Testing result for [1d] report ending GMT', start_of_today - timedelta(seconds=1))
 
-for record in dbresp:
-    if not record[3]:
+auth_header = {"Authorization": "Bearer " + bearer_token}
+prom = PrometheusConnect(url=target_prometheus, headers=auth_header, disable_ssl=True)
+response = postgres_execute("SELECT * FROM reports_human WHERE interval_end = '{}' and frequency = 'day'"
+                            .format(start_of_today), result=True)
+print('Testing result for [1d] report ending GMT', start_of_today - timedelta(seconds=1))
+sample = [['Namespace', 'isAccurate',
+           'Prometheus CPU Report', 'Curator CPU Report',
+           'Prometheus Memory Report', 'Curator Memory Report']]
+for row in response:
+    if not row[3]:
         continue
     memory_correct = False
     metric_data = prom.custom_query_range(
         "sum(sum(container_memory_usage_bytes{container!='POD', container!='', pod!='', namespace='"
-        + str(record[3]) + "'} / 1024 / 1024) by (node, namespace, pod)) by (namespace)",
+        + str(row[3]) + "'} / 1024 / 1024) by (node, namespace, pod)) by (namespace)",
         start_time=start_of_today - timedelta(days=1, hours=6),  # utc - 6 hrs
         end_time=start_of_today - timedelta(hours=6, seconds=1),  # end_time is inclusive, use 23:59:59
-        step=str(3600)  # 3600 * 24 seconds per day
+        step=str('1m')  # consistent with koku-metrics-operator step
     )
-    memory_result = sum(float(i[1]) for i in metric_data[0]['values']) / len(metric_data[0]['values'])
-    # memory_result = float(metric_data[0]['values'][-1][1])
-    if isclose(memory_result, record[7]):  # col 7. pod memory usage
+    try:
+        memory_result = sum(float(i[1]) for i in metric_data[0]['values']) / len(metric_data[0]['values'])
+    except IndexError as e:
+        print('[INFO] no matching namespace: {} in prometheus'.format(row[3]))
+        continue
+    if isclose(memory_result, row[7]):  # col 7. pod memory usage
         memory_correct = True
     else:
-        # print(metric_data)
-        print(colored('[ERROR] namespace {} memory expected {} but got {}'.format(record[3], memory_result
-                                                                           , record[7]), 'red'))
+        print(colored('[ERROR] namespace {} memory expected {} but got {}'.format(row[3], memory_result
+                                                                                  , row[7]), 'red'))
     metric_data = prom.custom_query(
         "sum(sum(rate(container_cpu_usage_seconds_total{container!='POD',container!='',pod!='', namespace='"
-        + str(record[3]) + "'}[1d] offset " + offset_str + ")) BY (pod, namespace, node)) BY (namespace) * 1000",
-        # start_time=start_of_today - timedelta(days=1, hours=6),  # utc - 6 hrs
-        # end_time=start_of_today - timedelta(hours=6, seconds=1),  # end_time is inclusive, use 23:59:59
-        # step=str(3600)  # 3600 * 24 seconds per day
+        + str(row[3]) + "'}[1d] offset " + offset_str + ")) BY (pod, namespace, node)) BY (namespace) * 1000",
     )
-    # cpu_result = sum(float(i[1]) for i in metric_data[0]['values']) / len(metric_data[0]['values'])
-    cpu_result = float(metric_data[0]['value'][-1])
-    if memory_correct and isclose(cpu_result, record[4]):  # col 4. pod cpu usage
-        print(colored(f'[PASS] cpu/memory test pass {record[3]}', 'green'))
+    try:
+        cpu_result = float(metric_data[0]['value'][-1])
+    except IndexError as e:
+        print('[INFO] no matching namespace: {} in prometheus'.format(row[3]))
+        continue
+    if memory_correct and isclose(cpu_result, row[4]):  # col 4. pod cpu usage
+        print(colored(f'[PASS] cpu/memory test pass {row[3]}', 'green'))
     elif memory_correct:
-        # print(metric_data)
-        print(colored('[ERROR] namespace {} cpu expected {} but got {}'.format(record[3], cpu_result
-                                                                           , record[4]), 'red'))
+        print(colored('[ERROR] namespace {} cpu expected {} but got {}'.format(row[3], cpu_result
+                                                                           , row[4]), 'red'))
+    sample.append([row[3], '✅' if memory_correct and isclose(cpu_result, row[4]) else '❌', cpu_result, row[4],
+                   memory_result, row[7]])
+
+# save result as markdown
+sample.sort(key=lambda x: (x[1] == 'isAccurate', x[1]), reverse=True)
+markdown = str("| ")
+for e in sample[0]:
+    to_add = " " + str(e) + str(" |")
+    markdown += to_add
+markdown += "\n"
+
+markdown += '|'
+for i in range(len(sample[0])):
+    markdown += str("-------------- | ")
+markdown += "\n"
+for entry in sample[1:]:
+    markdown += str("| ")
+    for e in entry:
+        to_add = str(e) + str(" | ")
+        markdown += to_add
+    markdown += "\n"
+with open('sanity_check_result.md', 'w+') as fd:
+    fd.write(markdown)
+
